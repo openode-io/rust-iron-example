c4cbe5acc9d31c80fec1be3913eba1d2a2fc78e0
Martin Levesque <levesque.martin@gmail.com>
2018-11-22 20:22:26 -0500

    tested on openode

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,22 @@
-
-
-
 extern crate iron;
-extern crate router;
-extern crate rustc_serialize;
+extern crate params;
 
 use iron::prelude::*;
 use iron::status;
-use router::Router;
-use rustc_serialize::json;
-use std::io::Read;
-use std::sync::{Arc, Mutex};
-use std::env;
+use params::{Params, Value};
 
-#[derive(RustcEncodable, RustcDecodable)]
-struct Greeting {
-    msg: String
+fn handler(req: &mut Request) -> IronResult<Response> {
+    let params_map = req.get_ref::<Params>().unwrap();
+    let name = match params_map.find(&["name"]) {
+        Some(&Value::String(ref s)) => { s.clone() },
+        _ => String::from("world")
+    };
+
+    Ok(Response::with(
+        (status::Ok, format!("Hello, {}!", name))
+    ))
 }
 
 fn main() {
-    let greeting = Arc::new(Mutex::new(Greeting { msg: "Hello, World".to_string() }));
-    let greeting_clone = greeting.clone();
-
-    let mut router = Router::new();
-
-    router.get("/", move |r: &mut Request| {
-        hello_world(r, &greeting.lock().unwrap())
-    }, "get");
-
-    router.post("/set", move |r: &mut Request| {
-        set_greeting(r, &mut greeting_clone.lock().unwrap())
-    }, "set");
-
-    fn hello_world(_: &mut Request, greeting: &Greeting) -> IronResult<Response> {
-        let payload = json::encode(&greeting).unwrap();
-        Ok(Response::with((status::Ok, payload)))
-    }
-
-    // Receive a message by POST and play it back.
-    fn set_greeting(request: &mut Request, greeting: &mut Greeting) -> IronResult<Response> {
-        let mut payload = String::new();
-        request.body.read_to_string(&mut payload).unwrap();
-        *greeting = json::decode(&payload).unwrap();
-        Ok(Response::with(status::Ok))
-    }
-
-    let port = env!("PORT");
-    Iron::new(router).http(format!("localhost:{}", port)).unwrap();
+    Iron::new(handler).http("0.0.0.0:80").unwrap();
 }
